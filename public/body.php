e9bb221bea74f2039954efd637ed1421e834ee6d
Mehrdad <100048050+Mehrdadnka@users.noreply.github.com>
2023-01-11 18:59:18 +0330

    Update body.php

--- public/body.php
+++ public/body.php
@@ -1,89 +1,90 @@
-<div class="right">
-    <div class="top">
-        <button id="menu-btn">
-            <span class="material-icons-round">menu</span>
-        </button>
-        <div class="theme-toggler">
-            <span class="material-icons-round active">light_mode</span>
-            <span class="material-icons-round">dark_mode</span>
-        </div>
-        <div class="profile">
-            <div class="info">
-                <p>Hey, <b>Mehrdad</b></p>
-                <small class="text-muted">Admin</small>
+<div class="container">
+    <div class="right">
+        <div class="top">
+            <button id="menu-btn">
+                <span class="material-icons-round">menu</span>
+            </button>
+            <div class="theme-toggler">
+                <span class="material-icons-round active">light_mode</span>
+                <span class="material-icons-round">dark_mode</span>
             </div>
-            <div class="profile-photo">
-                <img src="images/profile1.png" alt="profile-img">
-            </div>
-        </div>
-    </div>
-    <?php
-        /*
-            ===============================================================================
-            ===============================================================================
-            =================================END OF TOP====================================
-            ===============================================================================
-            ===============================================================================
-        */
-    ?>
-    <div class="recent-updates">
-        <h2>Recent Updates</h2>
-        <div class="updates">
-            <div class="update">
-                <div class="profile-photo">
-                    <img src="images/profile2.jpg" alt="profile-img">
-                </div>
-                <div class="message">
-                    <p><b>Mike Tyson</b> received his order of
-                    Night lion tech GPS drone</p>
-                    <small class="text-muted">2 Minets ago</small>
-                </div>
-            </div>
-            <div class="update">
-                <div class="profile-photo">
-                    <img src="images/avatar3.png" alt="profile-img">
-                </div>
-                <div class="message">
-                    <p><b>Joodie Rabinson</b> received his order of
-                    Night lion tech GPS drone</p>
-                    <small class="text-muted">2 Minets ago</small>
-                </div>
-            </div>
-            <div class="update">
-                <div class="profile-photo">
-                    <img src="images/avatar4.jpg" alt="profile-img">
-                </div>
-                <div class="message">
-                    <p><b>Patric Amondsen</b> received his order of
-                    Night lion tech GPS drone</p>
-                    <small class="text-muted">2 Minets ago</small>
-                </div>
-            </div>
-        </div>
-    </div>
-    <?php
-        /*
-            =============================================================================== 
-            ===============================================================================
-            =============================END OF RECENT UPDATES=============================
-            ===============================================================================
-            ===============================================================================
-        */
-    ?>
-    <div class="sales-analytics">
-        <h2>Sales Analytics</h2>
-        <div class="item online">
-            <div class="icon">
-                <span class="material-icons-round">shopping_cart</span>
-            </div>
-            <div class="right">
+            <div class="profile">
                 <div class="info">
-                    <h3>ONLINE ORDERS </h3>
-                    <small class="text-muted">Last 24 Hours</small>
+                    <p>Hey, <b>Mehrdad</b></p>
+                    <small class="text-muted">Admin</small>
                 </div>
-                <h5 class="success"> &nbsp +39%</h5>
-                <h3> <br>3849</h3>
+                <div class="profile-photo">
+                    <img src="images/profile1.png" alt="profile-img">
+                </div>
             </div>
+        </div>
+        <?php
+            /*
+                ===============================================================================
+                ===============================================================================
+                =================================END OF TOP====================================
+                ===============================================================================
+                ===============================================================================
+            */
+        ?>
+        <div class="recent-updates">
+             <h2>Recent Updates</h2>
+            <div class="updates">
+                <div class="update">
+                    <div class="profile-photo">
+                         <img src="images/profile2.jpg" alt="profile-img">
+                    </div>
+                    <div class="message">
+                         <p><b>Mike Tyson</b> received his order of
+                         Night lion tech GPS drone</p>
+                        <small class="text-muted">2 Minets ago</small>
+                    </div>
+                </div>
+                <div class="update">
+                    <div class="profile-photo">
+                        <img src="images/avatar3.png" alt="profile-img">
+                    </div>
+                    <div class="message">
+                        <p><b>Joodie Rabinson</b> received his order of
+                        Night lion tech GPS drone</p>
+                        <small class="text-muted">2 Minets ago</small>
+                    </div>
+                </div>
+                <div class="update">
+                    <div class="profile-photo">
+                        <img src="images/avatar4.jpg" alt="profile-img">
+                    </div>
+                    <div class="message">
+                        <p><b>Patric Amondsen</b> received his order of
+                        Night lion tech GPS drone</p>
+                        <small class="text-muted">2 Minets ago</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php
+            /*
+                =============================================================================== 
+                ===============================================================================
+                =============================END OF RECENT UPDATES=============================
+                ===============================================================================
+                ===============================================================================
+            */
+        ?>
+        <div class="sales-analytics">
+            <h2>Sales Analytics</h2>
+            <div class="item online">
+                <div class="icon">
+                    <span class="material-icons-round">shopping_cart</span>
+                </div>
+                <div class="right">
+                    <div class="info">
+                        <h3>ONLINE ORDERS </h3>
+                        <small class="text-muted">Last 24 Hours</small>
+                    </div>
+                    <h5 class="success"> &nbsp +39%</h5>
+                    <h3> <br>3849</h3>
+                </div>
             </div>
             <div class="item offline">
                 <div class="icon">
